f64b7b5cda4adfb5863d2ef7bd90f2e399ccb929
Arturas <arturasg08@gmail.com>
2019-09-17 14:55:39 +0300

    basic windows added for future work

---
+++ Assets/Script/TouchScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchScript : MonoBehaviour
+{
+
+    public bool isTouched = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
