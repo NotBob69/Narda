e4fa24a0243027a6be5e82e2153066e57b203177
Arturas <arturasg08@gmail.com>
2019-10-01 13:27:53 +0300

    made some progress making pet ai
    made stats, and changable text

---
+++ Assets/Script/PetScript/ChangePetText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ChangePetText : MonoBehaviour
+{
+    // maybe change to gameobject.find instead of putting manualy every text field atleast at start when everything changes often
+    public TextMeshProUGUI healthTxt;
+    public TextMeshProUGUI cleanlessTxt;
+    public TextMeshProUGUI hungerTxt;
+    public TextMeshProUGUI hapinessTxt;
+
+
+
+    public void ChangePetStatsTxt(float health, float cleanless, float hunger, float hapiness) {
+
+        healthTxt.text = health.ToString();
+        cleanlessTxt.text = cleanless.ToString();
+        hungerTxt.text = hunger.ToString();
+        hapinessTxt.text = hapiness.ToString();
+    }
+
+    public void ChangeText(TextMeshProUGUI textField, float number)
+    {
+        textField.text = number.ToString();
+
+    }
+
+
+}
