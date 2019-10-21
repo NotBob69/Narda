66e10bcd611b8359cb51acb89398dbc423523062
Arturas <arturasg08@gmail.com>
2019-10-21 15:37:52 +0300

    fixing spongeMechanics

--- Assets/Script/UI  script/PetCleaningScript.cs
+++ Assets/Script/UI  script/PetCleaningScript.cs
@@ -12,12 +12,7 @@ public class PetCleaningScript : MonoBehaviour
 
     public void CleanPet()
     {
-        /*
-        this.gameObject.GetComponent<PetStats>().cleanless = 100;
-        this.gameObject.GetComponent<PetStats>().UpdateStats();
-        */
-
-
+ 
         if(!isSpongeOut)
         Instantiate(sponge, cleanButton.transform.position,new Quaternion(0,0,0,0));
 
