4b6fbe7574af57e7654c4d474c00ad92f17a9378
Arturas <arturasg08@gmail.com>
2019-10-21 14:04:36 +0300

    made item prefabs

---
+++ Assets/Script/itemData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemData : MonoBehaviour
+{
+
+    public ItemScript item;
+
+
+    public int id;
+    public ItemScript.ItemType type;
+    public float itemCost;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        id = item.itemId;
+        gameObject.GetComponent<SpriteRenderer>().sprite = item.itemSprite;
+        type = item.type;
+        itemCost = item.itemCost;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
