b8d726bfb1bfce82683a25bb662fe4bb60fbcec6
unknown <deiviuxs97@gmail.com>
2019-10-21 14:22:04 +0300

    New login scene

---
+++ Assets/Script/Login.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+using System.Text.RegularExpressions;
+
+public class Login : MonoBehaviour
+{
+    public GameObject username;
+    public GameObject password;
+    private string Username;
+    private string Password;
+    private String[] Lines;
+
+    public void LoginButton()
+    {
+        bool UN = false;
+        bool PW = false;
+        if (Username != "")
+        {
+            if(System.IO.File.Exists(@"C:\Users\PC\Desktop\UnityLogin\" + Username + ".txt"))
+            {
+                UN = true;
+                Lines = System.IO.File.ReadAllLines(@"C:\Users\PC\Desktop\UnityLogin\" + Username + ".txt");
+            }
+            else
+            {
+                Debug.LogWarning("Username Invalid");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Username Field Empty");
+        }
+        if(Password != "")
+        {
+            if(System.IO.File.Exists(@"C:\Users\PC\Desktop\UnityLogin\" + Username + ".txt"))
+            {
+                String pass = Lines[1];
+                if (Password == pass)
+                {
+                    PW = true;
+                }
+                else
+                {
+                    Debug.LogWarning("Password Incorrect");
+                }
+            }
+            else
+            {
+                Debug.LogWarning("Password Invalid");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Password Field Empty");
+        }
+        if(UN == true&&PW == true)
+        {
+            username.GetComponent<InputField>().text = "";
+            password.GetComponent<InputField>().text = "";
+            print("Login Sucessful");
+            UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
+            //SceneManager.LoadLevel("MainScene");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (Username != "" && Password != "")
+            {
+                LoginButton();
+            }
+        }
+        Username = username.GetComponent<InputField>().text;
+        Password = password.GetComponent<InputField>().text;
+    }
+}
---
+++ Assets/Script/Register.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Text.RegularExpressions;
+
+public class Register : MonoBehaviour{
+    public GameObject username;
+    public GameObject password;
+    public GameObject confPassword;
+    private string Username;
+    private string Password;
+    private string ConfPassword;
+    private string form;
+
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void RegisterButton()
+    {
+        bool UN = false;
+        bool PW = false;
+        bool CPW = false;
+
+        if (Username != "")
+        {
+            if (!System.IO.File.Exists(@"C:\Users\PC\Desktop\UnityLogin\" + Username + ".txt"))
+            {
+                UN = true;
+            }
+            else
+            {
+                Debug.LogWarning("Username Taken");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Username field Empty");
+        }
+        if (Password != "")
+        {
+            if(Password.Length > 5)
+            {
+                PW = true;
+            }
+            else
+            {
+                Debug.LogWarning("Password Must Be Atleast 6 Characters Long");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Password Field Empty");
+        }
+        if(ConfPassword != "")
+        {
+            if (ConfPassword == Password)
+            {
+                CPW = true;
+            }
+            else
+            {
+                Debug.LogWarning("Passwords Dont Match");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Confirm Password Field Empty");
+        }
+        if(UN == true&&PW == true&&CPW == true)
+        {
+            form = (Username + "\n" + Password);
+            System.IO.File.WriteAllText(@"C:\Users\PC\Desktop\UnityLogin\" + Username + ".txt", form);
+            username.GetComponent<InputField>().text = "";
+            password.GetComponent<InputField>().text = "";
+            confPassword.GetComponent<InputField>().text = "";
+            print("Registration Sucessful");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if(Username != ""&&Password != ""&&ConfPassword!= "")
+            {
+                RegisterButton();
+            }
+        }
+        Username = username.GetComponent<InputField>().text;
+        Password = password.GetComponent<InputField>().text;
+        ConfPassword = confPassword.GetComponent<InputField>().text;
+    }
+}
