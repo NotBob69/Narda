bdf75c8eadac188a3b015616cb4d92a534bedaf5
neda mikelionyte <mikelionyte.neda@gmail.com>
2019-11-05 01:28:00 +0200

    Button in shop

--- Assets/Script/Key.cs
+++ Assets/Script/Key.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (gameObject.GetComponent<ActionScript>().isTouched)
+        {
+            GameObject.Find("Gameplay manager").GetComponent<PetStats>().happiness += 5;
+        }
     }
 }
---
+++ Assets/Script/Sellbutton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sellbutton : MonoBehaviour
+{
+    public void Button_Click()
+    {
+        Debug.Log("Sold items ");
+    }
+}
