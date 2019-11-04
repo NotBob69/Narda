45d44fa4b502b6c97ccdf6723ddd04c1d447e46c
neda mikelionyte <mikelionyte.neda@gmail.com>
2019-11-05 01:20:58 +0200

    SHOP managment

---
+++ Assets/Script/ButtonHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{
+  public void Button_Click()
+    {
+        Debug.Log("Purchase items ");
+    }
+}
